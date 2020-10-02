620034bc6619947033e8850d7f8bdb9bef1c0d76
Partast1 <andersa1989@gmail.com>
2020-10-02 08:45:31 +0200

    Initial commit

---
+++ Device.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempClassLibrary
+{
+    public class Device
+    {
+        private float temp;
+        private float humidity;
+        public float Temp
+        {
+            get { return temp; }
+            set { temp = value; }
+        }
+        public float Humidity
+        {
+            get { return humidity; }
+            set { humidity = value; }
+        }
+        public Device(float temp, float humidity)
+        {
+            Temp = temp;
+            Humidity = humidity;
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempClassLibrary
+{
+    public class Room
+    {
+        //Variables
+        private int roomNumber;
+        private string hallIndicator;
+        public int RoomNumber
+        {
+            get { return roomNumber; }
+            set { roomNumber = value; }
+        }
+        public string HallIndicator
+        {
+            get { return hallIndicator; }
+            set { hallIndicator = value; }
+        }
+        public Room(int roomNumber, string hallIndicator)
+        {
+            RoomNumber = roomNumber;
+            HallIndicator = hallIndicator;
+        }
+    }
+}
